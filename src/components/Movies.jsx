cc76cd3f69cbb2b433df0316e969f0377fcebeb5
Vishal21121 <vs84077000@gmail.com>
2023-06-16 09:53:55 +0530

    added: necessary messsage

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,26 +1,40 @@
+// importing the useEffect and useState hooks
 import React, { useEffect, useState } from 'react'
+// importing card component
 import Card from './Card'
+// importing useNavigate from "react-router-dom"
 import { useNavigate } from "react-router-dom"
+// importing the account and database from "../appwrite/appwriteConfig"
 import { account, databases } from '../appwrite/appwriteConfig'
+// importing the Navbar component
 import Navbar from './Navbar'
+// importing the Query clas from "appwrite"
 import { Query } from 'appwrite'
 
 const Movies = () => {
+    // initializing the useNavigate
     const navigate = useNavigate()
+    // setting the initial state of the data
     const [data, setData] = useState("")
+    // setting the initial state of the isSignin
     const [isSignin, setIsSignin] = useState(false)
+    // setting the initial state of the userDetails
     const [userDetails, setUserDetails] = useState()
+    // setting the initial state of the wishlist value
     const [wishlistNav, setWishListNav] = useState("Wishlist")
 
+    // using the fetchData method to fetch the value from the api of tmdb
     const fetchData = async () => {
+         // using the fetch api to make the get request to fetch the movies from the tmdb using the api key whihc we are accessing from the .env file.
         let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1`)
         let val = await response.json();
-        // console.log(val);
+        // setting the value of the movie using the setData method
         setData(val["results"]);
     }
 
     // checking whether the user is logged in or not
     const isUserLoggedIn = () => {
+        // getting the user details
         const getData = account.get()
         getData.then(
             // if logged in then perform below actions
@@ -29,16 +43,16 @@ const Movies = () => {
                 setUserDetails(response)
                 // setting the signin to true means that the user is logged in
                 setIsSignin(true)
-                // console.log("userDetails: ", userDetails);
             },
             function (error) {
                 console.log(error);
             }
         )
     }
-
+// calling the isUserLoggedIn method when the web page loads
     useEffect(isUserLoggedIn, [])
 
+    // calling the fetchData method when the web page loads
     useEffect(() => {
         fetchData()
         console.log("data: ", data)
@@ -47,8 +61,11 @@ const Movies = () => {
     // handling the signout function
     const handleLogOut = async () => {
         try {
+            // deleting the session of the user inoder to log him out
             await account.deleteSession("current")
+            // setting the isSignIn to false
             setIsSignin(false)
+            // navigating to the home page
             navigate("/")
             fetchData()
         } catch (error) {
@@ -58,9 +75,11 @@ const Movies = () => {
 
     // handling the add movie option
     const addMovie = (e) => {
+        // if the user is not siggned in then navigate the user to the login page
         if (!isSignin) {
             navigate("/login")
         } else {
+            // else gathering the data to store in the appwrite database
             const addButton = e.target
             let id = addButton.parentElement.parentElement.getAttribute("id")
             let popularity = addButton.previousElementSibling.innerText.split(':')[1];
@@ -77,7 +96,7 @@ const Movies = () => {
                 release_date: release_date,
                 popularity: popularity
             }
-            // console.log("data: ", userDetails["$id"]);
+            // creating the document inside the desired collection using the database id, collection id, id of the document and data. It will return a promise.
             const promise = databases.createDocument(import.meta.env.VITE_DATABASE_ID, import.meta.env.VITE_COLLECTION_ID, id, data)
             promise.then(
                 function (response) {
@@ -91,16 +110,22 @@ const Movies = () => {
 
     }
 
+    // handling the wislist button click
     const handleWishlist = () => {
+        // if wishlistNav state has Previous value then making it to Wishlist
         if (wishlistNav === "Previous") {
             setWishListNav("Wishlist");
+            // calling the fetch method to fetch the movies from the tmdb database using the api
             fetchData()
         } else {
+            // fetching the movies from the appwrite database using the database id, collection id and using the Query class of available in appwrite
             const promise = databases.listDocuments(import.meta.env.VITE_DATABASE_ID, import.meta.env.VITE_COLLECTION_ID, [Query.equal("id", userDetails["$id"])])
             promise.then(
                 function (response) {
                     console.log(response);
+                    // if everything goes welll swicth the wishlistNav value to Previos 
                     setWishListNav("Previous")
+                    // set the data using the setData method
                     setData(response.documents)
 
                 },
@@ -114,11 +139,13 @@ const Movies = () => {
 
     // handle the search bar functionality
     const searchMovie = async (name) => {
+        // using the fetch api to get the desired movie using get method and movie name.
         let res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1&include_adult=false&query=${name}`, {
             method: "GET",
         })
         let val = await res.json()
         console.log("value: ",val['results'][0]);
+        // setting the value using the setData method
         setData(val['results'][0])
     }
 
